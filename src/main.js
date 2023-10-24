4159d441fc0f3286d29cf27e6acf272167fbff91
Xyves <xodrixx@protonmail.com>
2023-10-24 19:04:24 +0200

    Affix navbar

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-const container = document.querySelector(".bg-container");
-export default function generateMainContent() {
-  const navBarBg = document.createElement("section");
-  container.appendChild(navBarBg);
+const container = document.querySelector(".container");
+export default function () {
+  const main = document.createElement("section");
+  const mainText = document.createElement("div");
+  const mainPhoto = document.createElement("div");
+  const pizzaImg = document.createElement("img");
+  const textHeader = document.createElement("h2");
+  textHeader.textContent = "Lunch Specials";
+  main.classList.add("section");
+  textHeader.classList.add("mainHeader");
+  pizzaImg.href = "sr";
+  mainPhoto;
+  container.appendChild(main);
+  main.appendChild(mainText);
+  main.appendChild(mainPhoto);
+  mainText.appendChild(textHeader);
 }
--- src/nav-bar.js
+++ src/nav-bar.js
@@ -1,40 +1,37 @@
 const container = document.querySelector(".bg-container");
 
-export default function generateAside() {
+export default function () {
   function createNav() {
+    const menuItems = ["Home", "About", "Menu", "Gallery"];
+    const urls = ["/dist/home", "/dist/about", "/dist/menu", "/dist/gallery"];
     const navBar = document.createElement("nav");
     const ul = document.createElement("ul");
-    const home = document.createElement("li");
-    const about = document.createElement("li");
-    const menu = document.createElement("li");
-    const gallery = document.createElement("li");
     let Logo = document.createElement("img");
     const logoBox = document.createElement("div");
+    const arrow = document.createElement("div");
+    container.src = "src/img/pizza.jpg";
     Logo.src = "../src/img/Jenos_logo.png";
 
-    navBar.classList.add("h-1/4", "p-6", "m-full", "bg-orange-700", "text-4xl");
-    ul.classList.add("flex", "justify-between", "block");
-    home.classList.add("inline", "ml-25", "block", "p-1");
-    about.classList.add("about", "mb-4", "inline", "block", "p-1");
-    menu.classList.add("menu", "mb-4", "block", "p-1");
-    gallery.classList.add("gallery", "block", "p-1");
     Logo.classList.add();
-    logoBox.classList.add("py-0", "px-15", "w-20", "h-20");
-
-    about.textContent = "about";
-    menu.textContent = "menu";
-    gallery.textContent = "gallery";
-    home.textContent = "home";
+    logoBox.classList.add("logo");
+    arrow.classList.add("arrow");
 
     navBar.appendChild(logoBox);
     logoBox.appendChild(Logo);
     container.appendChild(navBar);
     navBar.appendChild(ul);
-    ul.appendChild(home);
-    ul.appendChild(about);
-    ul.appendChild(menu);
-    ul.appendChild(gallery);
 
+    container.appendChild(arrow);
+    for (let i = 0; i < menuItems.length; i++) {
+      let li = document.createElement("li");
+      li.classList.add(menuItems[i]);
+      let a = document.createElement("a");
+      a.href = urls[i];
+      a.textContent = menuItems[i];
+
+      li.appendChild(a);
+      ul.appendChild(li);
+    }
     const icons = document.createElement("div");
     icons.classList.add("icons");
   }
