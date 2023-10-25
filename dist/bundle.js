38cdf6435f6d868f1b9a77a2d3ea3eded95debac
Xyves <xodrixx@protonmail.com>
2023-10-25 21:38:04 +0200

    Add new text to main section

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,69 @@
-(()=>{"use strict";const e=document.querySelector(".bg-container"),t=document.querySelector(".container");console.log("123"),document.body.onload=(function(){const t=["Home","About","Menu","Gallery"],n=["/dist/home","/dist/about","/dist/menu","/dist/gallery"],d=document.createElement("nav"),c=document.createElement("ul");let a=document.createElement("img");const l=document.createElement("div"),o=document.createElement("div");e.src="src/img/pizza.jpg",a.src="../src/img/Jenos_logo.png",a.classList.add(),l.classList.add("logo"),o.classList.add("arrow"),d.appendChild(l),l.appendChild(a),e.appendChild(d),d.appendChild(c),e.appendChild(o);for(let e=0;e<t.length;e++){let d=document.createElement("li");d.classList.add(t[e]);let a=document.createElement("a");a.href=n[e],a.textContent=t[e],d.appendChild(a),c.appendChild(d)}document.createElement("div").classList.add("icons")}(),void function(){const e=document.createElement("section"),n=document.createElement("div"),d=document.createElement("div"),c=document.createElement("img"),a=document.createElement("h2");a.textContent="Lunch Specials",e.classList.add("section"),a.classList.add("mainHeader"),c.href="sr",t.appendChild(e),e.appendChild(n),e.appendChild(d),n.appendChild(a)}())})();
+(() => {
+  "use strict";
+  const e = document.querySelector(".bg-container"),
+    t = document.querySelector(".container");
+  console.log("123"),
+    (document.body.onload =
+      ((function () {
+        const t = ["Home", "About", "Menu", "Gallery"],
+          n = ["/dist/home", "/dist/about", "/dist/menu", "/dist/gallery"],
+          d = document.createElement("nav"),
+          c = document.createElement("ul");
+        let a = document.createElement("img");
+        const i = document.createElement("div"),
+          l = document.createElement("div");
+        (e.src = "src/img/pizza.jpg"),
+          (a.src = "../src/img/Jenos_logo.png"),
+          a.classList.add(),
+          i.classList.add("logo"),
+          l.classList.add("arrow"),
+          d.appendChild(i),
+          i.appendChild(a),
+          e.appendChild(d),
+          d.appendChild(c),
+          e.appendChild(l);
+        for (let e = 0; e < t.length; e++) {
+          let d = document.createElement("li");
+          d.classList.add(t[e]);
+          let a = document.createElement("a");
+          (a.href = n[e]),
+            (a.textContent = t[e]),
+            d.appendChild(a),
+            c.appendChild(d);
+        }
+        document.createElement("div").classList.add("icons");
+      })(),
+      void (function () {
+        const e = document.createElement("section"),
+          n = document.createElement("div"),
+          d = document.createElement("div"),
+          c = document.createElement("img"),
+          a = document.createElement("h2"),
+          i = document.createElement("p"),
+          l = document.createElement("p"),
+          s = document.createElement("p"),
+          o = document.createElement("p"),
+          m = document.createElement("p");
+        (a.textContent = "Lunch Specials"),
+          (i.textContent = "2 slices of cheese pizza with a drink $4.60"),
+          (l.textContent = "2 slices of cheese pizza with a drink $4.60"),
+          (s.textContent = "2 slices of cheese pizza with a drink $4.60"),
+          (o.textContent = "2 slices of cheese pizza with a drink $4.60"),
+          (m.textContent = "2 slices of cheese pizza with a drink $4.60"),
+          l.classList.add("grey"),
+          o.classList.add("grey"),
+          n.classList.add("textContainer"),
+          (a.textContent = "Lunch Specials"),
+          e.classList.add("section"),
+          a.classList.add("mainHeader"),
+          (c.href = "sr"),
+          t.appendChild(e),
+          e.appendChild(n),
+          e.appendChild(d),
+          n.appendChild(i),
+          n.appendChild(l),
+          n.appendChild(s),
+          n.appendChild(o),
+          n.appendChild(m);
+      })()));
+})();
--- src/main.js
+++ src/main.js
@@ -1,17 +1,38 @@
 const container = document.querySelector(".container");
 export default function () {
   const main = document.createElement("section");
-  const mainText = document.createElement("div");
+  const textContainer = document.createElement("div");
   const mainPhoto = document.createElement("div");
   const pizzaImg = document.createElement("img");
   const textHeader = document.createElement("h2");
+
+  const p1 = document.createElement("p");
+  const p2 = document.createElement("p");
+  const p3 = document.createElement("p");
+  const p4 = document.createElement("p");
+  const p5 = document.createElement("p");
+
+  textHeader.textContent = "Lunch Specials";
+  p1.textContent = "2 slices of cheese pizza with a drink $4.60";
+  p2.textContent = "2 slices of cheese pizza with a drink $4.60";
+  p3.textContent = "2 slices of cheese pizza with a drink $4.60";
+  p4.textContent = "2 slices of cheese pizza with a drink $4.60";
+  p5.textContent = "2 slices of cheese pizza with a drink $4.60";
+
+  p2.classList.add("gray");
+  p4.classList.add("graaay");
+  textContainer.classList.add("textContainer");
   textHeader.textContent = "Lunch Specials";
   main.classList.add("section");
   textHeader.classList.add("mainHeader");
   pizzaImg.href = "sr";
   mainPhoto;
   container.appendChild(main);
-  main.appendChild(mainText);
+  main.appendChild(textContainer);
   main.appendChild(mainPhoto);
-  mainText.appendChild(textHeader);
+  textContainer.appendChild(p1);
+  textContainer.appendChild(p2);
+  textContainer.appendChild(p3);
+  textContainer.appendChild(p4);
+  textContainer.appendChild(p5);
 }
